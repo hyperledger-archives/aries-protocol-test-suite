f6455495a6a53a6ce69c9bb19e62696e0d278f1b
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-06-24 15:50:23 -0400

    Merge in and outbound transport files
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ agent_core/transport/http.py
@@ -0,0 +1,92 @@
+""" Define HTTP Transports """
+import asyncio
+import logging
+
+from aiohttp import web
+import aiohttp
+
+from transport import InboundConnection, OutboundConnection,\
+        ConnectionCapabilities, InboundTransport, CannotOpenConnection
+
+LOGGER = logging.getLogger(__name__)
+
+
+class HTTPInboundTransport(InboundTransport):
+    """ HTTP Inbound Transport """
+    async def accept(self, **options):
+        routes = [
+            web.post('/indy', self.post_handle)
+        ]
+        app = web.Application()
+        app.add_routes(routes)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        server = web.TCPSite(runner=runner, port=options['port'])
+        LOGGER.info('Starting on localhost: %s', options['port'])
+        await server.start()
+
+    async def post_handle(self, request):
+        """ Post handler """
+        msg = await request.read()
+        conn = HTTPInboundConnection(msg)
+        await self.connection_queue.put(conn)
+
+        try:
+            await asyncio.wait_for(conn.wait(), 5)
+        except asyncio.TimeoutError:
+            await conn.close()
+
+        if conn.new_msg:
+            return web.Response(body=conn.new_msg)
+
+        raise web.HTTPAccepted()
+
+
+class HTTPInboundConnection(InboundConnection):
+    """ HTTP Inbound Connection """
+    def __init__(self, msg):
+        super().__init__(ConnectionCapabilities.RECV)
+        self.msg = msg
+        self.new_msg = None
+
+    async def recv(self):
+        self.set_send()
+        yield self.msg
+
+    async def send(self, msg):
+        self.new_msg = msg
+        await self.close()
+
+
+class HTTPOutConnection(OutboundConnection):
+    """ HTTP Outbound Connection """
+    def __init__(self, endpoint):
+        super().__init__(ConnectionCapabilities.SEND)
+        self.endpoint = endpoint
+        self.new_msg = None
+
+    @classmethod
+    async def open(cls, **service):
+        if 'serviceEndpoint' not in service:
+            raise CannotOpenConnection()
+
+        return cls(service['serviceEndpoint'])
+
+    async def send(self, msg):
+        async with aiohttp.ClientSession() as session:
+            headers = {'content-type': 'application/ssi-agent-wire'}
+            async with session.post(
+                    self.endpoint,
+                    data=msg,
+                    headers=headers
+                        ) as resp:
+
+                if resp.status != 202:
+                    self.new_msg = await resp.read()
+                    self.set_recv()
+                else:
+                    self.close()
+
+    async def recv(self):
+        self.close()
+        yield self.new_msg
