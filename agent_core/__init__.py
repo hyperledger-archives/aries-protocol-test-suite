d95d22f854e9c54535537a404ca8fb3ae6a1c5f0
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-06-25 10:11:15 -0400

    Agent.from_config finished
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- agent_core/__init__.py
+++ agent_core/__init__.py
@@ -2,6 +2,7 @@
 
     Defines a simple public API for creating and running an agent.
 """
+import asyncio
 from typing import Sequence
 
 from ariespython import wallet, error
@@ -9,7 +10,17 @@ from ariespython import wallet, error
 from .config import Config
 from .dispatcher import Dispatcher
 from .conductor import Conductor
-from .transport import InboundTransport
+from .transport import InboundTransport, http, std, websocket, http_plus_ws
+
+
+def inbound_transport_str_to_module(transport_str):
+    """ Map transport config strings to modules """
+    return {
+        'http': http.HTTPInboundTransport,
+        'std': std.StdInboundTransport,
+        'ws': websocket.WebSocketInboundTransport,
+        'http+ws': http_plus_ws.HTTPPlusWebSocketTransport
+    }[transport_str]
 
 
 class Agent:
@@ -54,17 +65,17 @@ class Agent:
         conductor = Conductor(wallet_handle)
 
         # Create inbound transports
+        transports = []
         for transport in config.inbound_transports:
-            pass
+            transport_mod = inbound_transport_str_to_module(transport)
+            transports.append(
+                transport_mod(conductor.connection_queue)
+            )
 
         # Create dispatcher
         dispatcher = Dispatcher()
 
-        return cls(wallet_handle, config, conductor, [], dispatcher)
-
-
-    async def open_transports(self):
-        pass
+        return cls(wallet_handle, config, conductor, transports, dispatcher)
 
     async def send(self, *args, **kwargs):
         self.conductor.send(*args, **kwargs)
