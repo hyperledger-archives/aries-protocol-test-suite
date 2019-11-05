c3d1b2faee6d45afe20558bfc074805d6ca6d125
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-11-05 14:56:39 -0500

    Replace agent-core with aries-staticagent-python
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ config.py
@@ -0,0 +1,16 @@
+import toml
+
+def load_config(config_file: str):
+    """Load configuration from toml file."""
+    return toml.load(config_file)['config']
+
+def default():
+    """Return a default config dictionary."""
+    return {
+        'host': 'localhost',
+        'port': 3000,
+        'subject': {
+            'endpoint': 'http://localhost:3001'
+        },
+        'features': []
+    }
