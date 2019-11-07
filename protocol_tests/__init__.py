40b37aad3dace7cb1fc9c27c49d10e24cbf6d4f7
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-11-07 16:48:59 -0500

    Break out schema helpers into schema.py
    Add Slot mechanism for reusing schema objects for constructing instances
    of that schema.
    
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- protocol_tests/__init__.py
+++ protocol_tests/__init__.py
@@ -1,101 +1,10 @@
 """ Protocol Test Helpers """
 from contextlib import contextmanager
-from functools import reduce
 from typing import Dict, Iterable, Union
-from warnings import warn
 import hashlib
 import json
 
 from aries_staticagent import StaticConnection, crypto
-
-from voluptuous import Schema
-from voluptuous.error import Invalid, MultipleInvalid
-
-
-class ValidationError(Exception):
-    """When errors on validation"""
-    def __init__(self, error: Invalid):
-        if isinstance(error, MultipleInvalid):
-            super().__init__(
-                'Multiple errors found during validation:\n\t' +
-                ('\n\t'.join(
-                    [
-                        str(error)
-                        for error in error.errors
-                    ]
-                ))
-            )
-        else:
-            super().__init__(
-                str(error)
-            )
-
-
-class UnexceptedMessageKey(Warning):
-    """Warning raised when an unexpected property is found in a message."""
-
-
-def invalid_into_unexpected(error):
-    path = ' @ message[%s]' % ']['.join(map(repr, error.path)) \
-        if error.path else ''
-    warn(
-        'Unexpected Message key found{}'.format(path),
-        UnexceptedMessageKey
-    )
-
-
-def is_extra_key_error(error):
-    return error.error_message == 'extra keys not allowed'
-
-
-def MessageSchema(schema, warn_on_extra=True):
-    """
-    Wrap validation for messages to ensure they are passed as dictionaries.
-
-    Passing as messages results in errors as the validator attempts to mutate
-    nested objects into messages which fails when no `@type` attribute is
-    available.
-
-    Also wrap errors into collected Validation error and raise extra key errors
-    as warnings instead of exceptions.
-    """
-    __tracebackhide__ = True
-    validator = Schema(schema)
-
-    def _ensure_dict_and_validate(msg):
-        try:
-            return validator(dict(msg))
-        except MultipleInvalid as error:
-            def split(acc, error):
-                extra_errors, not_extra_errors = acc
-                if is_extra_key_error(error):
-                    extra_errors.append(error)
-                else:
-                    not_extra_errors.append(error)
-                return (extra_errors, not_extra_errors)
-
-            extra_errors, not_extra_errors = reduce(
-                split, error.errors, ([], [])
-            )
-
-            # Apply invalid_into_unexpected to each error for extra keys
-            list(map(invalid_into_unexpected, extra_errors))
-
-            if not_extra_errors:
-                # Re-raise the rest
-                raise ValidationError(
-                    MultipleInvalid(not_extra_errors)
-                )
-        except Invalid as error:
-            if warn_on_extra:
-                if is_extra_key_error(error):
-                    invalid_into_unexpected(error)
-                else:
-                    raise ValidationError(error)
-            else:
-                raise ValidationError(error)
-
-    return _ensure_dict_and_validate
 
 
 def _recipients_from_packed_message(packed_message: bytes) -> Iterable[str]:
