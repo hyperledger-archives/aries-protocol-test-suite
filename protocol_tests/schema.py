f1515f0b3cd628ff6a8c6ad0c6cea1f3dbb84df9
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-11-15 11:08:39 -0500

    Remove warnings; add "developer notes" option
    That shows log messages greater than or equal WARNING
    
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- protocol_tests/schema.py
+++ protocol_tests/schema.py
@@ -1,6 +1,6 @@
 """Schema helpers."""
 from functools import reduce
-from warnings import warn
+import logging
 
 from voluptuous import Schema
 from voluptuous.error import Invalid, MultipleInvalid
@@ -25,16 +25,11 @@ class ValidationError(Exception):
             )
 
 
-class UnexceptedMessageKey(Warning):
-    """Warning raised when an unexpected property is found in a message."""
-
-
-def invalid_into_unexpected(error):
+def log_unexpected(error):
     path = ' @ message[%s]' % ']['.join(map(repr, error.path)) \
         if error.path else ''
-    warn(
-        'Unexpected Message key found{}'.format(path),
-        UnexceptedMessageKey
+    logging.getLogger(__name__).warning(
+        'Unexpected Message key found{}'.format(path)
     )
 
 
@@ -53,19 +48,19 @@ class MessageSchema():
     Also wrap errors into collected Validation error and raise extra key errors
     as warnings instead of exceptions.
     """
-    __slots__ = ('schema', 'validator', 'warn_on_extra')
+    __slots__ = ('schema', 'validator', 'allow_extra')
 
-    def __init__(self, schema, warn_on_extra=True):
+    def __init__(self, schema, allow_extra=True):
         self.schema = schema
         self.validator = Schema(schema)
-        self.warn_on_extra = warn_on_extra
+        self.allow_extra = allow_extra
 
     def __call__(self, msg):
         __tracebackhide__ = True
         try:
             return self.validator(dict(msg))
         except MultipleInvalid as error:
-            if self.warn_on_extra:
+            if self.allow_extra:
                 def split(acc, error):
                     extra_errors, not_extra_errors = acc
                     if is_extra_key_error(error):
@@ -78,8 +73,7 @@ class MessageSchema():
                     split, error.errors, ([], [])
                 )
 
-                # Apply invalid_into_unexpected to each error for extra keys
-                list(map(invalid_into_unexpected, extra_errors))
+                list(map(log_unexpected, extra_errors))
 
                 if not_extra_errors:
                     # Re-raise the rest
@@ -89,9 +83,9 @@ class MessageSchema():
             else:
                 raise ValidationError(error)
         except Invalid as error:
-            if self.warn_on_extra:
+            if self.allow_extra:
                 if is_extra_key_error(error):
-                    invalid_into_unexpected(error)
+                    log_unexpected(error)
                 else:
                     raise ValidationError(error)
             else:
