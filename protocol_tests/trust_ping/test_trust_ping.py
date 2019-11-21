d0993ee944ad3bfe05c2784ccb1da0f1ee78a0ef
Adam Burdett <burdettadam@gmail.com>
2019-11-21 14:44:14 -0700

    working trust ping
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- protocol_tests/trust_ping/test_trust_ping.py
+++ protocol_tests/trust_ping/test_trust_ping.py
@@ -1,5 +1,3 @@
-""" Demonstrate testing framework. """
-
 import pytest
 
 from aries_staticagent import Message, crypto
@@ -8,37 +6,34 @@ from aries_staticagent.mtc import (
     DESERIALIZE_OK, NONREPUDIATION
 )
 from reporting import meta
-from ..schema import MessageSchema, Optional, Required
+from ..schema import MessageSchema
+from voluptuous import Optional, Required
 
 
 @pytest.mark.asyncio
 @meta(protocol='trust_ping', version='0.1', role='*', name='trust-ping-with-response-requested-true')
 async def test_trust_ping_with_response_requested_true(backchannel):
-    """ 
-    @type, @id, response_requested -> required. 
-    ~timing.out_time, ~timing.expires_time, ~timing.delay_milli, comment -> optional.
-    """
 
     expected_trust_ping_schema = MessageSchema({
-        Required("@type"): "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping",
-        Required("@id"): str,
+        "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping",
+        "@id": str,
         Optional("~timing"): {
             Optional("out_time"): str,
             Optional("expires_time"): str,
             Optional("delay_milli"): int
         },
         Optional("comment"): str,
-        Required("response_requested"): bool
+        "response_requested": bool
     })
 
     expected_trust_pong_schema = MessageSchema({
-        Required("@type"): "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping_response",
+        "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping_response",
         Optional("@id"): str,
-        Required("~thread"): {"thid": "518be002-de8e-456e-b3d5-8fe472477a86"},
+        "~thread": {"thid": str},
         Optional("~timing"): {
-            Optional("in_time"): str, 
+            Optional("in_time"): str,
             Optional("out_time"): str
-            },
+        },
         Optional("comment"): str
     })
 
@@ -54,13 +49,17 @@ async def test_trust_ping_with_response_requested_true(backchannel):
     )
 
     print('Received message:', trust_pong.pretty_print())
-
+    # is auth crypted?----------------------
     assert trust_pong.mtc[
         CONFIDENTIALITY | INTEGRITY | AUTHENTICATED_ORIGIN | DESERIALIZE_OK
     ]
     assert not trust_pong.mtc[NONREPUDIATION]
+    # --------------------------------------
+    # are you, you and am I, me?
     assert trust_pong.mtc.ad['sender_vk'] == crypto.bytes_to_b58(
         backchannel.their_vk)
-    assert trust_pong.mtc.ad['recip_vk'] == crypto.bytes_to_b58(backchannel.my_vk)
-
-    assert expected_trust_pong_schema.validate(trust_pong)
+    assert trust_pong.mtc.ad['recip_vk'] == crypto.bytes_to_b58(
+        backchannel.my_vk)
+    # --------------------------------------
+    assert expected_trust_pong_schema(trust_pong)
+    assert trust_pong['~thread']['thid'] == trust_ping['@id']
